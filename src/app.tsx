1cb92cf9609e081124314a1c7f2aece536b2bb35
Lakin <lakin.awel@aastustudent.edu.et>
2026-07-29 22:45:47 +0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,25 @@
-n
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Join from './pages/Join';
+import Donate from './pages/Donate';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/donate" element={<Donate />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
